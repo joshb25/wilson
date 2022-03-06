daee756943c6b779aec0082baac714834c852d20
peppermint21 <jbanas1896@gmail.com>
2022-03-05 20:04:38 -0600

    testing10

--- src/imageData.js
+++ src/imageData.js
@@ -1,195 +1,195 @@
 const imageData=[
     {
         id: 1000,
-         url: "/weatherImages/clear03.jpg"
+         url: "public/weatherImages/clear03.jpg"
     },
     {
         id: 1003,
-         url: "/weatherImages/partly_cloudy.jpg"
+         url: "public/weatherImages/partly_cloudy.jpg"
     },
      {
         id: 1006,
-         url: "/weatherImages/cloudy04.jpg"
+         url: "public/weatherImages/cloudy04.jpg"
     },
      {
         id: 1009,
-         url: "/weatherImages/overcast.jpg"
+         url: "public/weatherImages/overcast.jpg"
     },
      {
         id: 1030,
-         url: "/weatherImages/mist.jpg"
+         url: "public/weatherImages/mist.jpg"
     },
      {
         id: 1063,
-         url: "/weatherImages/rain02.jpg"
+         url: "public/weatherImages/rain02.jpg"
     },
      {
         id: 1066,
-         url: "/weatherImages/snow02.jpg"
+         url: "public/weatherImages/snow02.jpg"
     },
      {
         id: 1069,
-         url: "/weatherImages/freeze02.jpg"
+         url: "public/weatherImages/freeze02.jpg"
     },
      {
         id: 1072,
-         url: "/weatherImages/freeze.jpg"
+         url: "public/weatherImages/freeze.jpg"
     },
      {
         id: 1087,
-         url: "/weatherImages/thunder.jpg"
+         url: "public/weatherImages/thunder.jpg"
     },
      {
         id: 1114,
-         url: "/weatherImages/heavySnow.jpg"
+         url: "public/weatherImages/heavySnow.jpg"
     },
      {
         id: 1117,
-         url: "/weatherImages/blizzard.jpg"
+         url: "public/weatherImages/blizzard.jpg"
     },
      {
         id: 1135,
-         url: "/weatherImages/fog.jpg"
+         url: "public/weatherImages/fog.jpg"
     },
      {
         id: 1147,
-         url: "/weatherImages/freezeFog.jpg"
+         url: "public/weatherImages/freezeFog.jpg"
     },
      {
         id: 1150,
-         url: "/weatherImages/rain.jpg"
+         url: "public/weatherImages/rain.jpg"
     },
      {
         id: 1153,
-         url: "/weatherImages/rain.jpg"
+         url: "public/weatherImages/rain.jpg"
     },
      {
         id: 1168,
-         url: "/weatherImages/freeze05.jpg"
+         url: "public/weatherImages/freeze05.jpg"
     },
      {
         id: 1171,
-         url: "/weatherImages/freeze05.jpg"
+         url: "public/weatherImages/freeze05.jpg"
     },
      {
         id: 1180,
-         url: "/weatherImages/lightRain02.jpg"
+         url: "public/weatherImages/lightRain02.jpg"
     },
      {
         id: 1183,
-         url: "/weatherImages/lightRain02.jpg"
+         url: "public/weatherImages/lightRain02.jpg"
     },
      {
         id: 1186,
-         url: "/weatherImages/rain03.jpg"
+         url: "public/weatherImages/rain03.jpg"
     },
      {
         id: 1189,
-         url: "/weatherImages/rain03.jpg"
+         url: "public/weatherImages/rain03.jpg"
     },
      {
         id: 1192,
-         url: "/weatherImages/heavyRain.jpg"
+         url: "public/weatherImages/heavyRain.jpg"
     },
      {
         id: 1195,
-         url: "/weatherImages/heavyRain.jpg"
+         url: "public/weatherImages/heavyRain.jpg"
     },
      {
         id: 1198,
-         url: "/weatherImages/rain04.jpg"
+         url: "public/weatherImages/rain04.jpg"
     },
     {
         id: 1201,
-         url: "/weatherImages/rain04.jpg"
+         url: "public/weatherImages/rain04.jpg"
     },
     {
         id: 1204,
-         url: "/weatherImages/rain03.jpg"
+         url: "public/weatherImages/rain03.jpg"
     },
     {
         id: 1207,
-         url: "/weatherImages/rain03.jpg"
+         url: "public/weatherImages/rain03.jpg"
     },
     {
         id: 1210,
-         url: "/weatherImages/snow02.jpg"
+         url: "public/weatherImages/snow02.jpg"
     },
     {
         id: 1213,
-         url: "/weatherImages/snow02.jpg"
+         url: "public/weatherImages/snow02.jpg"
     },
     {
         id: 1216,
-         url: "/weatherImages/snow02.jpg"
+         url: "public/weatherImages/snow02.jpg"
     },
     {
         id: 1219,
-         url: "/weatherImages/snow02.jpg"
+         url: "public/weatherImages/snow02.jpg"
     },
     {
         id: 1222,
-         url: "/weatherImages/snow04.jpg"
+         url: "public/weatherImages/snow04.jpg"
     },
     {
         id: 1225,
-         url: "/weatherImages/heavySnow.jpg"
+         url: "public/weatherImages/heavySnow.jpg"
     },
     {
         id: 1237,
-         url: "./wilson/weatherImages/freeze02.jpg"
+         url: "public/weatherImages/freeze02.jpg"
     },
     {
         id: 1240,
-         url: "/weatherImages/lightRain02.jpg"
+         url: "public/weatherImages/lightRain02.jpg"
     },
     {
         id: 1243,
-         url: "/weatherImages/heavyRain.jpg"
+         url: "public/weatherImages/heavyRain.jpg"
     },
     {
         id: 1246,
-         url: "/weatherImages/heavyRain.jpg"
+         url: "public/weatherImages/heavyRain.jpg"
     },
     {
         id: 1249,
-         url: "/weatherImages/lightRain02.jpg"
+         url: "public/weatherImages/lightRain02.jpg"
     },
     {
         id: 1252,
-         url: "/weatherImages/heavyRain.jpg"
+         url: "public/weatherImages/heavyRain.jpg"
     },
     {
         id: 1255,
-         url: "/weatherImages/snow02.jpg"
+         url: "public/weatherImages/snow02.jpg"
     },
     {
         id: 1258,
-         url: "/weatherImages/snow04.jpg"
+         url: "public/weatherImages/snow04.jpg"
     },
     {
         id: 1261,
-         url: "/weatherImages/freeze02.jpg"
+         url: "public/weatherImages/freeze02.jpg"
     },
     {
         id: 1264,
-         url: "/weatherImages/snow04.jpg"
+         url: "public/weatherImages/snow04.jpg"
     },
     {
         id: 1273,
-         url: "/weatherImages/rain02.jpg"
+         url: "public/weatherImages/rain02.jpg"
     },
     {
         id: 1276,
-         url: "/weatherImages/heavyRain.jpg"
+         url: "public/weatherImages/heavyRain.jpg"
     },
     {
         id: 1279,
-         url: "/weatherImages/snow02.jpg"
+         url: "public/weatherImages/snow02.jpg"
     },
     {
         id: 1282,
-         url: "/weatherImages/snow05.jpg"
+         url: "public/weatherImages/snow05.jpg"
     }
 
 ]
